715e5164e230040931005bede0d33da5a9d79aa3
Mohammed85 <mohammed346520@hotmail.com>
2023-07-19 01:46:36 +0300

    Changed one var in one file in one line as Fred is lazy

--- mythos/App.axaml.cs
+++ mythos/App.axaml.cs
@@ -38,7 +38,7 @@ public partial class App : Application
         {
             desktop.MainWindow = new MainWindow
             {
-                DataContext = new MainView()
+                DataContext = new MainViewModel()
             };
         }
         else if (ApplicationLifetime is ISingleViewApplicationLifetime singleViewPlatform)
